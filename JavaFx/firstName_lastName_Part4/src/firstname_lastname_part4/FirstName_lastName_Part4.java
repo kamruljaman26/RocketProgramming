c895670c0dccc63d470169e35a54aa6699b99bab
Kamrul Jaman <kamruljaman@Kamruls-MacBook-Pro.local>
2022-01-27 18:47:35 +0600

    Saif: Modify part4 and add Thread lock

--- JavaFx/firstName_lastName_Part4/src/firstname_lastname_part4/FirstName_lastName_Part4.java
+++ JavaFx/firstName_lastName_Part4/src/firstname_lastname_part4/FirstName_lastName_Part4.java
@@ -1,13 +1,22 @@
 package firstname_lastname_part4;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class FirstName_lastName_Part4 {
 
     public static void main(String[] args) {
+        final int MAX_T = 2;
+        ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
 
-        MyThread myThread = new MyThread();
-        Thread thread = new Thread(myThread);
-        thread.start();
+        for (int i = 0; i < 100; i++) {
+            Random random = new Random();
+            MyThread thread = new MyThread(random.nextInt(), random.nextInt(), 65);
+            pool.execute(thread);
+        }
 
+        pool.shutdown();
     }
-
 }
--- JavaFx/firstName_lastName_Part4/src/firstname_lastname_part4/MyThread.java
+++ JavaFx/firstName_lastName_Part4/src/firstname_lastname_part4/MyThread.java
@@ -4,21 +4,28 @@ package firstname_lastname_part4;
 import java.util.Random;
 
 public class MyThread implements Runnable{
-        
-    
+
+    // should have 3 instant variable
+    private int hours;
+    private int minutes;
+    private int second;
+
+    // default constructor
+    public MyThread(int hours, int minutes, int second) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.second = second;
+    }
 
     @Override
     public void run() {
-        
-        Random random = new Random();
-        
-        int randomhour,randommunite,randomsec;
-        randomhour=random.nextInt();
-        randommunite = random.nextInt();
-        randomsec = random.nextInt();
-        
-        CalculateTime.timeSinceMidnight(randomhour,randomsec,randommunite);
-     
+        try {
+            int time = CalculateTime.timeSinceMidnight(hours,minutes,second);
+            System.out.println("Time Since Midnight: "+time+"s");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Error in MyThread");
+        }
     }
-    
 }
